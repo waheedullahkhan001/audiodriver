e37f949cb7e021769fc49c0460def274864afbd6
Waheedullah Khan <waheedullahkhan1250@gmail.com>
2025-03-25 14:08:48 +0500

    refactor MainActivity layout to enhance service control UI and add loading indicator

--- app/src/main/java/com/freedom/audiodriver/MainActivity.kt
+++ app/src/main/java/com/freedom/audiodriver/MainActivity.kt
@@ -7,7 +7,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -21,6 +26,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,6 +47,7 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
+import com.spr.jetpack_loading.components.indicators.PacmanIndicator
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -63,123 +70,161 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun Main(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(horizontal = 24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        val context = LocalContext.current
-        val scope = rememberCoroutineScope()
-        val permissionState = rememberPermissionState(
-            Manifest.permission.RECORD_AUDIO
-        )
-        var isRunning by remember {
-            mutableStateOf(
-                MainService.isServiceRunningInForeground(
-                    context, MainService::class.java
-                )
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val permissionState = rememberPermissionState(
+        Manifest.permission.RECORD_AUDIO
+    )
+    var isRunning by remember {
+        mutableStateOf(
+            MainService.isServiceRunningInForeground(
+                context, MainService::class.java
             )
-        }
+        )
+    }
+    var isLoading by remember { mutableStateOf(false) }
 
-        if (permissionState.status.isGranted) {
-            Column(
-                modifier = Modifier.fillMaxHeight(0.5f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-            ) {
-                Card(
-                    elevation = CardDefaults.cardElevation(
-                        defaultElevation = 6.dp
-                    ), modifier = Modifier.size(width = 240.dp, height = 120.dp)
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            if (permissionState.status.isGranted) {
+                Column(
+                    modifier = Modifier.fillMaxHeight(0.5f),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(16.dp),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
+                    Card(
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 6.dp
+                        ),
+                        modifier = Modifier.size(width = 240.dp, height = 120.dp),
+                        border = BorderStroke(
+                            2.dp,
+                            if (isRunning) MaterialTheme.colorScheme.primary
+                            else MaterialTheme.colorScheme.primary.copy(alpha = 0f)
+                        ),
                     ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
 
-                        Row(
-                            Modifier.padding(vertical = 12.dp),
-                        ) {
-                            Text(
-                                text = if (isRunning) "Service is running "
-                                else "Service is stopped ",
-                                textAlign = TextAlign.Center,
-                            )
-                        }
-                        Button(
-                            modifier = Modifier.fillMaxWidth(),
-                            onClick = {
-                                isRunning = MainService.isServiceRunningInForeground(
-                                    context, MainService::class.java
+                            Row(
+                                Modifier.padding(vertical = 12.dp),
+                            ) {
+                                Text(
+                                    text = if (isRunning) "Service is running "
+                                    else "Service is stopped ",
+                                    textAlign = TextAlign.Center,
                                 )
-                            },
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.Refresh,
-                                contentDescription = "Refresh",
-                            )
+                            }
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                onClick = {
+                                    isRunning = MainService.isServiceRunningInForeground(
+                                        context, MainService::class.java
+                                    )
+                                },
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Refresh,
+                                    contentDescription = "Refresh",
+                                )
+                            }
                         }
                     }
                 }
-            }
-            Column(
-                modifier = Modifier.fillMaxHeight(0.5f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Button(
-                    modifier = Modifier.fillMaxWidth(), onClick = {
-                        val intent = Intent(context, MainService::class.java).apply {
-                            action = MainService.Actions.START.name
-                        }
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            context.startForegroundService(intent)
-                        } else {
-                            context.startService(intent)
-                        }
-                        scope.launch {
-                            delay(3000)
-                            isRunning = MainService.isServiceRunningInForeground(
-                                context, MainService::class.java
-                            )
-                        }
-                    }) {
-                    Text("Start")
+                Column(
+                    modifier = Modifier.fillMaxHeight(0.5f),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Button(
+                        enabled = !isRunning,
+                        modifier = Modifier.fillMaxWidth(), onClick = {
+                            val intent = Intent(context, MainService::class.java).apply {
+                                action = MainService.Actions.START.name
+                            }
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                context.startForegroundService(intent)
+                            } else {
+                                context.startService(intent)
+                            }
+                            scope.launch {
+                                isLoading = true
+                                delay(1000)
+                                isRunning = MainService.isServiceRunningInForeground(
+                                    context, MainService::class.java
+                                )
+                                isLoading = false
+                            }
+                        }) {
+                        Text("Start")
+                    }
+                    Button(
+                        enabled = isRunning,
+                        modifier = Modifier.fillMaxWidth(), onClick = {
+                            val intent = Intent(context, MainService::class.java).apply {
+                                action = MainService.Actions.STOP.name
+                            }
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                context.startForegroundService(intent)
+                            } else {
+                                context.startService(intent)
+                            }
+                            scope.launch {
+                                isLoading = true
+                                delay(3000)
+                                isRunning = MainService.isServiceRunningInForeground(
+                                    context, MainService::class.java
+                                )
+                                isLoading = false
+                            }
+                        }) {
+                        Text("Stop")
+                    }
                 }
-                Button(
-                    modifier = Modifier.fillMaxWidth(), onClick = {
-                        val intent = Intent(context, MainService::class.java).apply {
-                            action = MainService.Actions.STOP.name
-                        }
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            context.startForegroundService(intent)
-                        } else {
-                            context.startService(intent)
-                        }
-                        scope.launch {
-                            delay(3000)
-                            isRunning = MainService.isServiceRunningInForeground(
-                                context, MainService::class.java
-                            )
-                        }
-                    }) {
-                    Text("Stop")
-                }
-            }
-        } else {
-            val textToShow = if (permissionState.status.shouldShowRationale) {
-                "The microphone is important for this app. Please grant the permission."
             } else {
-                "Microphone permission required for this feature to be available. " + "Please grant the permission"
+                val textToShow = if (permissionState.status.shouldShowRationale) {
+                    "The microphone is important for this app. Please grant the permission."
+                } else {
+                    "Microphone permission required for this feature to be available. " + "Please grant the permission"
+                }
+                Text(
+                    textToShow, textAlign = TextAlign.Center
+                )
+                Button(onClick = { permissionState.launchPermissionRequest() }) {
+                    Text("Request permission")
+                }
             }
-            Text(
-                textToShow, textAlign = TextAlign.Center
-            )
-            Button(onClick = { permissionState.launchPermissionRequest() }) {
-                Text("Request permission")
+        }
+
+        if (isLoading) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = MaterialTheme.colorScheme.background.copy(alpha = 0.3f),
+                    )
+                    // This intercepts all touch events
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = { /* consume click */ }
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                PacmanIndicator(
+                    color = MaterialTheme.colorScheme.primary,
+                )
             }
         }
     }
